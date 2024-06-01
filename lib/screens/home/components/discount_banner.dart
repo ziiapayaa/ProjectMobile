dfbe23844e7127243fdfb9ae49a23617b7369c02
Joker223311 <yaakali33@gmail.com>
2024-06-02 01:16:40 +0700

    ft

---
+++ lib/screens/home/components/discount_banner.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class DiscountBanner extends StatelessWidget {
+  const DiscountBanner({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.all(20),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 16,
+      ),
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 3, 73, 51),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: const Text.rich(
+        TextSpan(
+          style: TextStyle(color: Colors.white),
+          children: [
+            TextSpan(text: "A Summer Surpise\n"),
+            TextSpan(
+              text: "Cashback 50%",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
